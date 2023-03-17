bd46088942e5f19ff67d105e803934b096569c11
root <root@HEL1UM-EB8470P.localdomain>
2023-03-18 01:56:17 +0300

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Program starts from first letter lowercase alphabet and loops from a-z
+*resets first letter to uppercase and loops from A-Z
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+char letter = 'a';
+
+while (letter <= 'z')
+{
+putchar(letter);
+letter++;
+}
+
+letter = 'A';
+
+while (letter <= 'Z')
+{
+putchar(letter);
+letter++;
+}
+
+puchar('\n');
+
+return (0);
+}
