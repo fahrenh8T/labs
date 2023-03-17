65893a9f3c50268f6ffef2738521af9830faa1aa
root <root@HEL1UM-EB8470P.localdomain>
2023-03-18 01:03:50 +0300

    1-last_digit.c updt

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,23 +16,18 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	last_digit = abs(n) % 5;
-
-	printf("Last digit of %d is %d\n", n, last_digit);
-
 	if (last_digit > 5)
 	{
-		printf("and is greater than 5\n");
+		printf("Last digit of %d is %d greater than 5\n", n, last_digit);
 	}
-	else if (last_digit == 0)
+	else if (last_digit < 6 && last_digit != 0)
 	{
-		printf("and is 0\n");
+		printf("Last digit of %d is %d is less than 6 and not 0\n", n, last_digit);
 	}
 	else
 	{
-		printf("and is less than 6 and not 0\n");
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	}
 
 	return (0);
 }
-
