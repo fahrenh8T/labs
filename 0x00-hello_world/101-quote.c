c368b5f51ad86d78b7cd111d077a4e9d7c759f31
root <root@HEL1UM-EB8470P.localdomain>
2023-03-21 02:32:13 +0300

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *Programme prints and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf('and that piece of art is useful" - Dora Korpar, 2015-10-19\n');
+
+return (0);
+}
