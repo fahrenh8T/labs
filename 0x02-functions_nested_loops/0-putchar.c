8c6c3ab0390f28c67df1475bef155a1f22f19bda
root <root@HEL1UM-EB8470P.localdomain>
2023-03-22 02:13:17 +0300

    0-putchar.c updt4

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - entry point
- * 
+ * prints _putchar, followed by a new line
  * Return: Always 0.
 */
 
 int main(void)
 {
-	char ch[] = "_putchar";
-	int i = 0;
-
-	while (ch[i] != '\0')
-	{
-		_putchar(ch[i]);
-		i++;
-	}
-	_putchar('\n');
+	_putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
 
 return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,1 @@
-#include <stdio.h>
-/**
- * main - entry point
- * program that prints _putchar, followed by a new line.
- * Return putchar
-*/
-int _putchar(char c)
-{
-return putchar (c);
-}
+int _putchar(char c);
