03e56e3df13256882a17b3cf3575ecd346e15928
root <root@HEL1UM-EB8470P.localdomain>
2023-03-21 03:29:18 +0300

    100-print_comb3.c updt

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+* main - Entry point
+* program that prints all possible different combinations of two digits
+* Return: Always 0 (success)
+*/
 int main(void)
 {
 int i, j;
