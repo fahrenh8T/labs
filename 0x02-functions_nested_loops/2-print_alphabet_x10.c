5a449670df5df6bcf32a991d4276ec501e3c5eb2
root <root@HEL1UM-EB8470P.localdomain>
2023-03-22 02:51:24 +0300

    2-print_alphabet_x10.c updt2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * prints 10 times the alphabet, in lowercase, followed by a new line.
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase, followed by a new line.
  * Return: Always 0 (success)
 */
 void print_alphabet_x10(void)
