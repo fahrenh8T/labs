1d2f94f02534976c14856eb86cf9ccedd9816d39
root <root@HEL1UM-EB8470P.localdomain>
2023-03-21 03:33:23 +0300

    102-print_comb5.c updt

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+* main - Entry point
+* program that prints all possible combinations of two two-digit numbers
+* Return: Always 0 (success)
+*/
 int main(void)
 {
 int i, j;
