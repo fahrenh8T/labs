8a25586410270fd11ff2d6b38b2ddbed08fb0481
root <root@HEL1UM-EB8470P.localdomain>
2023-03-21 03:32:12 +0300

    101-print_comb4.c updt

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+* main - Entry point
+* program that prints all possible different combinations of three digits
+* Return: Always 0 (success)
+*/
 int main(void)
 {
 int i, j, k;
