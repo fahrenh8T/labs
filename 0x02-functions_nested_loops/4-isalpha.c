17506cd995b8ddb88df6135c90e5a86dbb092eb8
root <root@HEL1UM-EB8470P.localdomain>
2023-03-22 03:17:51 +0300

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isapha -shows 1 if input is lowercase or uppercase letter. 0 if otherwise
+ * @c: the character in ASCII table
+ * Return: 1 for letter. 0 if otherwise.
+*/
+int _isalpha(int c)
+{
+if (c >= 65 && c <= 90) || (c >= 97 && c <= 122) 
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
+}
