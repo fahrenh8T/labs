a44771e69b4b6709a462394bf56b46b96187962b
root <root@HEL1UM-EB8470P.localdomain>
2023-03-22 05:53:17 +0300

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: integer starting point
+ * Return: 0
+*/
+void print_to_98(int n)
+{
+int i;
+if (n <= 98)
+{
+for (int i = n; i <= 98; i++)
+{
+_putchar(i / 10 + 48);
+_putchar(i % 10 + 48);
+_putchar(44);
+_putchar(32);
+}
+else
+{
+for (int i = n; i > 98; i--)
+{
+_putchar(i / 10 + 48);
+_putchar(i % 10 + 48);
+_putchar(44);
+_putchar(32);
+}
+}
+}
+_putchar(57);
+_putchar(56);
+_putchar('\n');
+}
