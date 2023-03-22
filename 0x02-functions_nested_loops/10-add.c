df5c9064d522d54bbbb10feb0298d431264945a0
root <root@HEL1UM-EB8470P.localdomain>
2023-03-22 05:02:53 +0300

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add -  adds two integers and returns the result
+ * @a: integer added to b
+ * @b: integer added to a
+ * Return: answer(a + b)
+*/
+int add(int a, int b)
+{
+int c;
+
+c = a + b;
+
+return (c);
+}
