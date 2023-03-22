44685e18a402f6835af07a29d40b97d0576673da
root <root@HEL1UM-EB8470P.localdomain>
2023-03-22 04:01:04 +0300

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - compute the absolute value of an integer
+ * @i: integer (int)
+ * Return: 0 (success)
+*/
+int _abs(int i)
+{
+if (i >= 0)
+{
+return (i);
+}
+else
+{
+return (i * -1);
+}
+}
