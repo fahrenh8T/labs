4a6b730981ed0f56dc7fa0481d490fc84a512105
root <root@HEL1UM-EB8470P.localdomain>
2023-03-24 05:02:16 +0300

    6-print_line.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -6,3 +9,5 @@ void print_most_numbers(void);
 int _putchar(char c);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int n);
+#endif /* MAIN_H */
