e5264480df769d8291da28cef1aa31d42125fecd
root <root@HEL1UM-EB8470P.localdomain>
2023-03-24 04:19:18 +0300

    3-print_numbers.c updt1

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
  * print_numbers - prints the numbers, from 0 to 9
- *
+ *Return: 0
 */
 void print_numbers(void)
 {
-	int i;
+	char i;
+
 	for (i = 48; i <= 57; i++)
 	{
-		_putchar(i + 48);
+		_putchar(i);
 	}
 	_putchar('\n');
 }
