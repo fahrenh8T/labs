5564e963e9de758e166f0475ea3de2d3dd514577
root <root@HEL1UM-EB8470P.localdomain>
2023-03-24 03:56:14 +0300

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * @c: result of multiplication
+ * Return: result of multiplication
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
