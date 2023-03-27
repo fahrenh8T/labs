65068daebc0870d6840e746fd0b340a6a61c8b60
root <root@HEL1UM-EB8470P.localdomain>
2023-03-28 02:05:06 +0300

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - pointers to an int and updates the value it points to 98.
+ *@n: value of integer to be updated
+ *  Return: 0
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
