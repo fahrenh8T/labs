f46ca42aabe066ddd5036e818a9df7be6f40466c
root <root@HEL1UM-EB8470P.localdomain>
2023-03-28 02:14:30 +0300

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of two integers.
+ *@a: swap to 42
+ *@b: swap to 98
+ * Return: 0
+*/
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
