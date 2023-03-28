dbc3e1b60d4cbce244ca99ba4f6f5d98db59caba
root <root@HEL1UM-EB8470P.localdomain>
2023-03-28 05:57:33 +0300

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+#define PASSWORD_LENGTH 7
+/**
+ * main - generates random valid passwords
+ * Return: 0
+*/
+
+int main(void)
+{
+	char passwd[PASSWORD_LENGTH + 1];
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+	srand(time(NULL));
+
+	for (int i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		passwd[i] = charset[rand() % (sizeof(charset) - 1)];
+	}
+	passwd[PASSWORD_LENGTH] = '\0';
+
+	printf("Generated Password: %s\n", passwd);
+
+	return (0);
+}
