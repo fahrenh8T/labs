6b59af832b96a80b27b23df1be058b9239bd9948
root <root@HEL1UM-EB8470P.localdomain>
2023-03-28 04:30:04 +0300

    2-strlen.c updt1

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,16 +2,16 @@
 /**
  * _strlen -return the length of a string
  * @s: pointer of the string
- * Return: 0
+ * Return: length of the string
 */
 int _strlen(char *s)
 {
 	int len = 0;
-	
-	while (*s !='\0')
+
+	while (*s != '\0')
 	{
 		len++;
 		s++;
 	}
-	return len;
+	return (len);
 }
