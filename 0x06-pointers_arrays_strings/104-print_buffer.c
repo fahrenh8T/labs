d05f9997d9c7c69b2e79639accde69d87cfa017d
root <root@HEL1UM-EB8470P.localdomain>
2023-04-01 01:04:11 +0300

    104-print_buffer.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,52 @@
+#include "main.h"
+/**
+ * print_buffer - prints a buffer
+ * @b: pointer of buffer
+ * @size: size of buffer in bytes
+ * Return: 0
+*/
+void print_buffer(char *b, int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		printf("\n");
+	}
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+			{
+				printf("%02x", (unsigned char)b[i + j]);
+			}
+			else
+			{
+				printf("  ");
+			}
+			if (j % 2 == 1)
+			{
+				printf(" ");
+			}
+		}
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+			{
+				if (b[i + j] >= ' ' && b[i + j] <= '~')
+				{
+					printf("%c", b[i + j]);
+				}
+				else
+				{
+					printf(".");
+				}
+			}
+		}
+
+		printf("\n");
+	}
+}
