d1d90c3e4f042cbe30a67346082341037354d901
root <root@HEL1UM-EB8470P.localdomain>
2023-03-31 05:45:45 +0300

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string to be capitalize
+ * Return: pointer to be capitalized string
+*/
+char *cap_string(char *s)
+{
+	int i, capitalize_next;
+
+	for (i = 0, capitalize_next = 1; s[i] != '\0'; i++)
+	{
+		if (capitalize_next && (s[i] >= 'a' && s[i] <= 'z'))
+		{
+			s[i] = s[i] - 'a' + 'A';
+		}
+		capitalize_next = 0;
+
+		switch (s[i])
+		{
+			case ' ':
+			case '\t':
+			case '\n':
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+			capitalize_next = 1;
+			break;
+			default:
+			break;
+		}
+			
+	}
+	return (s);
+}
