3692080d667cede19d3750a286cf3560993f342e
root <root@HEL1UM-EB8470P.localdomain>
2023-03-31 05:16:17 +0300

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: the destination string
+ * @src: the source string
+ * @n: maximum number of bytes from src to be concatenated
+ *
+ * Return: return pointer to resulting string dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = 0;
+	int i;
+
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+}
