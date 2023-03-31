f402ccaf182e8cab6c6ba6134fb331fc6ca53402
root <root@HEL1UM-EB8470P.localdomain>
2023-04-01 01:06:58 +0300

    104-print_buffer.c updt1

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -16,7 +16,6 @@ void print_buffer(char *b, int size)
 	for (i = 0; i < size; i += 10)
 	{
 		printf("%08x: ", i);
-
 		for (j = 0; j < 10; j++)
 		{
 			if (i + j < size)
@@ -46,7 +45,6 @@ void print_buffer(char *b, int size)
 				}
 			}
 		}
-
 		printf("\n");
 	}
 }
