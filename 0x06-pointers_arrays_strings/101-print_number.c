29349f4e15ce90b92bf3cbeca8fe065fb165c832
root <root@HEL1UM-EB8470P.localdomain>
2023-03-31 06:20:08 +0300

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: the integer to be printed
+*/
+void print_number(int n)
+{
+	int divisor = 1;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		n = -n;
+	}
+	while (n / divisor > 9)
+	{
+		divisor *= 10;
+	}
+	while (divisor != 0)
+	{
+		_putchar((n / divisor) + 48);
+		n %= divisor;
+		divisor /= 10;
+	}
+}
