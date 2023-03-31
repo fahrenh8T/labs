ad40b68c4858b307120e4eaaf1ad574061523163
root <root@HEL1UM-EB8470P.localdomain>
2023-03-31 06:05:01 +0300

    100-rot13.c updt1

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @s: string to be encoded
+ * Return: pointer to encoded string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; alphabet[j] != '\0'; j++)
+		{
+			if (s[i] == alphabet[j])
+			{
+				s[i] = rot13[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
