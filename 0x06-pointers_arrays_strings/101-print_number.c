e05bb88e20707fb2b4e6df970f7f6be31fcfa639
root <root@HEL1UM-EB8470P.localdomain>
2023-04-01 00:33:10 +0300

    101-print_number.c updt2

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,16 +7,14 @@ void print_number(int n)
 {
 	unsigned int num;
 
+	num = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		num = -n;
 	}
-	else
-	{
-		num = n;
-	}
-	if (num / 10)
+	if (num / 10 != 0)
 	{
 		print_number(num / 10);
 	}
