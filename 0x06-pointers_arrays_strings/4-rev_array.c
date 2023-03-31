85dc8c3f147c9d30ef9dca0e7d5fbfdf52795f4b
root <root@HEL1UM-EB8470P.localdomain>
2023-04-01 00:22:02 +0300

    2-strncpy.c updt1

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,6 @@
 #include "main.h"
  /**
- * reverse_array - reverses the content of an array of integers
+ * reverse_array - reverses the content of an array
  * @a: An array of integers.
  * @n: The number of elements in the array.
  */
