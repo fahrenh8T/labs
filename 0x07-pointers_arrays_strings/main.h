59c04bbe661477439145ac732157daf18af9b376
root <root@HEL1UM-EB8470P.localdomain>
2023-04-03 19:59:47 +0300

    mandatory tasks, testing codes

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+#endif /*MAIN_H*/
